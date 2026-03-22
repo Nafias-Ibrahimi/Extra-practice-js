c023ebebd981379f5e6415b9877ebacd607dc842
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-22 18:42:56 +0430

    add the inline and external js file

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ let width=5;
 let height=10;
 let area=width * height;
 console.log(`Area of the rectangle is: ${area}`);
+
+// 4. Use both inline and external JavaScript in an HTML file
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Exercise-one</title>
+// </head>
+// <body>
+//     <script src="/script.js"></script>
+//      <script>
+
+//     const num=[1,2,3,4,5,5];
+//         console.log(num[1]);
+//         console.log(num);
+//      </script>
+
+
+// </body>
+// </html>
+
+
