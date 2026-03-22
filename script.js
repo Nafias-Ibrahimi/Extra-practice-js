34f1cc0ac8d54c04f0d8fd91b766dc7da2628972
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-22 18:37:06 +0430

    the area of rectangle

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ console.log('name:' ,name);
 // Multiple lines comment in js
 // /* ... */ → for multi-line comments
 
-// const greet= ()=>{
-//     console.log('Hello World');
-// }
+const greet= ()=>{
+    console.log('Hello World');
+}
 
+// 3. Write a simple expression that calculates the area of a rectangle (width × height) and
+// print the result
 
+let width=5;
+let height=10;
+let area=width * height;
+console.log(`Area of the rectangle is: ${area}`);
