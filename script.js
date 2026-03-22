2100b546a00433d998b125646f9838b4b1e1c67e
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-22 18:30:17 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const name='Nafisa';
+console.log('name:' ,name);
+
+
+// 2. Add comments to explain each line of your code.
+//   console.log('Hello World');
+
+// This is a single-line comment
+// → for single-line comments
+
+// Multiple lines comment in js
+// /* ... */ → for multi-line comments
+
+// const greet= ()=>{
+//     console.log('Hello World');
+// }
+
+
