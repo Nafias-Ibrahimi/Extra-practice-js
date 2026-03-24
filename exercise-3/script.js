83eff9542ba56b25ca85d66e9a7ba9b20f94d974
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-24 09:56:56 +0430

    // Reference copy

--- exercise-3/script.js
+++ exercise-3/script.js
@@ -52,5 +52,52 @@ console.log(arr);
  arr.push(7);
  console.log(arr);
 
- 
+// 4. Write a function that takes a name and returns "Hello, name".
 
+function hello(name){
+   return 'Hello ,' + name;
+}
+console.log(hello('Ahmad'));
+console.log(hello('Ali'));
+
+
+//  5. Use typeof and instanceof to check types of different variables.
+typeof [1,2,3] //object
+typeof {a:1} // object
+typeof function(){} // function
+
+
+[1,2,3] instanceof Array //true
+new Date() instanceof Date //ture
+
+// 6. Demonstrate implicit type conversion in "5" + 5 and "5" - 2.
+
+console.log('5' +5);
+console.log('5' -2);
+
+
+// 7. Convert "123" to number, 123 to string, and 0 to boolean.
+
+const strNum='123';
+const nums=Number(strNum);
+console.log(nums, typeof nums);
+
+const realNum=123;
+const strs=String(realNum);
+console.log(strs ,typeof strs);
+
+
+const zero=0;
+const bools=Boolean(zero);
+console.log(bools ,typeof bool);
+
+// 8. Write code showing difference between primitive copy and reference copy.
+
+// Primitive copy
+let a=10;
+let b=a;
+b=20
+console.log('b:' ,b);
+console.log('a:' ,a);
+
+// Reference copy
