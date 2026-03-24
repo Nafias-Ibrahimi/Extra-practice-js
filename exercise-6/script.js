d6f987d34d6cc075683828c59b33d6ce2791e31e
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-24 15:18:39 +0430

    more exmaples

--- exercise-6/script.js
+++ exercise-6/script.js
@@ -20,3 +20,19 @@ const myCar={
 };
 Car(myCar);
 
+// 3. Create a nested object school with teachers and students. Access one teacher’s subject.
+
+const school={
+    teachers:[
+        {name:'Ahmad', subject:'Math'},
+        {name:'Nafisa' , subject:'English'},  
+    ],
+    students:[
+        {name:'Ali' , grade:10},
+        {name:'Maryam' , grade:11},
+        {name:'AliArsam', grade:12}
+    ]
+}
+
+console.log(`teacher and subject: ${school.teachers[0].name ,school.teachers[0].subject}`);
+console.log(`students and grade: ${school.students[0].name ,school.students[0].grade}`);
