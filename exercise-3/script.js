bd3297031bb6cd9d25b99ab45b78f1aefd643609
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-03-24 10:02:37 +0430

    created it new exercises

--- exercise-3/script.js
+++ exercise-3/script.js
@@ -101,3 +101,11 @@ console.log('b:' ,b);
 console.log('a:' ,a);
 
 // Reference copy
+
+let arr1=[1,2,3];
+let arr2=arr1;
+arr2.push(4);
+console.log('arr1 :', arr1);
+console.log('arr2 :', arr2);
+
+
